a4a3516f94fef3f3436df535482c59d3e487bde2
AAA4002911 <zeeshan4002911@gmail.com>
2022-11-04 07:35:17 +0530

    initial commit

---
+++ React/reactAssignment2/src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+import Paragraph from './Paragraph';
+
+const App = () => {
+  return (
+    <div id="main">
+      <div id="root">
+        <Paragraph/>
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
