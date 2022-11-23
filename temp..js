a06628ac1b236c52f052921dd8215cecf03770c6
AAA4002911 <zeeshan4002911@gmail.com>
2022-11-23 05:42:07 +0530

    initial commit

---
+++ temp..js
@@ -0,0 +1,11 @@
+// let a = [4,5,6,7].map(function(x) {return x+3}).filter(function(x) {return x%3}).map(function(x) {return x-3})
+// console.log(a);
+
+// var x = 0;
+// async function test() {
+//     x += await 2;
+//     console.log(x);
+// }
+// test();
+// x += 1;
+// console.log(x);
